3a4d2fdc3649e5a2160f781443a7bfe0463237fe
William H. Ju <william.h.ju@vanderbilt.edu>
2017-11-28 15:33:24 -0600

    Test labeling

--- course_scheduler.py
+++ course_scheduler.py
@@ -24,19 +24,28 @@ def main(argv):
     #     if key.program == 'CS':
     #         print(key, test[key])
     #print(test[("ANTH", "4154")].prereqs)
+    # Test 2
     #solution = williamju_scheduler.course_scheduler (test, [("CS", "2231"), ("CS", "3251"), ("CS", "statsprobability")], [('MATH', '2810'), ("MATH", "2820"), ("MATH", "3640")])
     #solution = williamju_scheduler.course_scheduler(test, [("CS", "major"), ("CS", "4269")], [])
-    solution = williamju_scheduler.course_scheduler(test, [("CS", "core"), ("CS", "1101")], [])
+    # Test 3
+    #solution = williamju_scheduler.course_scheduler(test, [("CS", "core"), ("CS", "1101")], [])
+    # Test 6
     #solution = williamju_scheduler.course_scheduler(test, [("CS", "major"), ('JAPN', '3891')], [('CS', '1101'), ('JAPN', '1101')])
+    # Test 8
+    #solution = williamju_scheduler.course_scheduler(test, [("CS", "major"), ('JAPN', '2201')], [])
+    # Test 7
     #solution = williamju_scheduler.course_scheduler(test, [("CS", "mathematics")], [])
     #solution = williamju_scheduler.course_scheduler(test, [("BME", "4900W")], [])
+    # Test 4
     #solution = williamju_scheduler.course_scheduler(test, [("CS", "major")], [('CS', '1101')])
-    #solution = williamju_scheduler.course_scheduler(test, [("CS", "major"), ('ANTH', '4345'), ('ARTS', '3600'), ('ASTR', '3600'), ('BME', '4500'), ('BUS', '2300'), ('CE', '3705')], [])
+    # Test 5
+    solution = williamju_scheduler.course_scheduler(test, [("CS", "major"), ('ANTH', '4345'), ('ARTS', '3600'), ('ASTR', '3600'), ('BME', '4500'), ('BUS', '2300'), ('CE', '3705')], [])
+    # Test 1
     #solution = williamju_scheduler.course_scheduler(test, [("CS", "1101")], [])
     #solution = williamju_scheduler.course_scheduler(test, [('ANTH', '4345'), ('ARTS', '3600'), ('BME', '4500'), ('BUS', '2300'), ('CE', '3705'), ('LAT', '3140'), ('JAPN', '3891')], [])
-    for course in solution:
-       print(course, solution[course])
-    #print(solution)
+    # for course in solution:
+    #    print(course, solution[course])
+    print(solution)
     # Prints the entire dictionary.
     #course_dictionary.print_dict(test)
     # print(test[('CS', 'open3')])
